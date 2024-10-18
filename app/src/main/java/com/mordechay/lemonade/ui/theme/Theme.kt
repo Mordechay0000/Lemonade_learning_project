08280cebf2517641a857e7dd3fdff2ef8ae2e93c
Mordechay <m8572998@gmail.com>
2024-10-18 12:19:26 +0300

    first commit

--- app/src/main/java/com/mordechay/lemonade/ui/theme/Theme.kt
+++ app/src/main/java/com/mordechay/lemonade/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.mordechay.lemonade.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
